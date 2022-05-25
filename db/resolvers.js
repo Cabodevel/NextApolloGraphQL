96348b841345982e51431eead6fd3aa2f83c2a8e
Carlos <carlosbolorino@hotmail.com>
2022-05-25 18:54:11 +0200

    authentication

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,9 +1,23 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+require('dotenv').config({path: 'variables.env'});
+
+const crearToken = (user, secret, expiresIn) =>{
+    
+    const {id, email, nombre, apellido } = user;
+
+    return jwt.sign({ id, email, nombre, apellido }, secret, { expiresIn });
+}
 
 const resolvers = {
     Query: {
-        obtenerCurso: () => "test"
+        obtenerUsuario: async(_, {token}) => {
+            const usuarioId = jwt.verify(token, process.env.SECRET);
+
+            return usuarioId;
+        }
     },
     Mutation: {
         nuevoUsuario: async (_, {input}) => {
@@ -27,6 +41,25 @@ const resolvers = {
                 console.log(error)
             }
 
+        },
+        autenticarUsuario: async(_, {input}) => {
+            const  {email, password } = input;
+            
+            const existeUsuario = await Usuario.findOne({email});
+
+            if(!existeUsuario){
+                throw new Error("El usuario no existe");
+            }
+
+            const passwordCorrecto = bcryptjs.compare(password, existeUsuario.password);
+
+            if(!passwordCorrecto){
+                throw new Error("Password incorrecto");
+            }
+
+            return {
+                token: crearToken(existeUsuario, process.env.SECRET, '24h')
+            }
         }
     }
 }
