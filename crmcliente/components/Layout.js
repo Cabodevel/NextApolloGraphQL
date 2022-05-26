b6fd0974ef8b804e69545f55ffc66d971298878d
Carlos <carlosbolorino@hotmail.com>
2022-05-26 19:37:47 +0200

    form crear cuenta

--- crmcliente/components/Layout.js
+++ crmcliente/components/Layout.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
 import Sidebar from './Sidebar'
+import { useRouter } from 'next/router'
 
 const Layout = ({children}) => {
+
+    const router = useRouter();
+
     return (  
         <>
             <Head>
@@ -10,7 +14,17 @@ const Layout = ({children}) => {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
                 <script src="https://cdn.tailwindcss.com"></script>
             </Head>
-            <div className='bg-gray-200 min-h-screen'>
+            {router.pathname.toLowerCase() === '/login' || router.pathname.toLowerCase() === '/nuevacuenta' ? 
+            (
+                <div className='bg-gray-800 min-h-screen flex flex-col justify-center'>
+                    <div>
+                        {children}
+                    </div>
+                  
+                </div>
+            ) : 
+            ( 
+                <div className='bg-gray-200 min-h-screen'>
                 <div className='flex min-h-screen'>
                     <Sidebar />
                     <main className='sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5'>
@@ -18,6 +32,8 @@ const Layout = ({children}) => {
                     </main>
                 </div>
             </div>
+            )}
+           
         </>
     );
 }
